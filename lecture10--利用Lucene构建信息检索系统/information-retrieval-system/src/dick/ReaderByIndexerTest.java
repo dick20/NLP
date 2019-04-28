9bc0e6eb5db9d07944808b6debbbd05034f1027e
dick20 <806179953@qq.com>
2019-04-28 15:00:29 +0800

    完成拼写检查

--- lecture10--利用Lucene构建信息检索系统/information-retrieval-system/src/dick/ReaderByIndexerTest.java
+++ lecture10--利用Lucene构建信息检索系统/information-retrieval-system/src/dick/ReaderByIndexerTest.java
@@ -1,18 +1,28 @@
 package dick;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Paths;
 
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.cn.smart.SmartChineseAnalyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.search.similarities.ClassicSimilarity;
 import org.apache.lucene.search.similarities.Similarity;
 import org.apache.lucene.search.similarities.TFIDFSimilarity;
+import org.apache.lucene.search.spell.Dictionary;
+import org.apache.lucene.search.spell.LuceneDictionary;
+import org.apache.lucene.search.spell.SpellChecker;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
  
@@ -35,9 +45,14 @@ public class ReaderByIndexerTest {
 		//建立索引查询器
 		IndexSearcher is=new IndexSearcher(reader);
 		
-		// 设置为TFIDF 排序
-		TFIDFSimilarity sim = new TFIDFSimilarity();
-		is.setSimilarity(similarity);
+		// 设置为TF/IDF 排序
+		ClassicSimilarity sim = new ClassicSimilarity();
+		// Implemented as sqrt(freq).
+		// sim.tf(reader.getSumDocFreq(q));
+		
+		// Implemented as log((docCount+1)/(docFreq+1)) + 1.
+		// sim.idf(reader.getSumDocFreq(q), reader.numDocs());
+		is.setSimilarity(sim);
 		//实例化分析器
 		Analyzer analyzer=new SmartChineseAnalyzer(); 
 		
@@ -50,7 +65,7 @@ public class ReaderByIndexerTest {
 		
 		//根据传进来的q查找
 		Query query=parser.parse(q);
-		
+
 		//计算索引开始时间
 		long start=System.currentTimeMillis();
 		
@@ -81,14 +96,79 @@ public class ReaderByIndexerTest {
 		reader.close();
 	}
 	
+	public static String[] checkWord(String queryWord){
+		//新索引目录
+		String spellIndexPath = "C:\\Users\\asus\\Desktop\\java\\information-retrieval-system\\newPath";
+		//已有索引目录
+		String oriIndexPath = "C:\\Users\\asus\\Desktop\\java\\information-retrieval-system\\index";
+
+		//拼写检查
+		try {
+			//目录
+			Directory directory = FSDirectory.open((new File(spellIndexPath)).toPath());
+
+			SpellChecker spellChecker = new SpellChecker(directory);
+
+			//以下几步用来初始化索引
+			IndexReader reader = DirectoryReader.open(FSDirectory.open((new File(oriIndexPath)).toPath()));
+			//利用已有索引
+			Dictionary dictionary = new LuceneDictionary(reader, LuceneConstants.CONTENTS);
+			
+			IndexWriterConfig config = new IndexWriterConfig(new SmartChineseAnalyzer());
+			spellChecker.indexDictionary(dictionary, config, true);
+			
+			int numSug = 5;
+			String[] suggestions = spellChecker.suggestSimilar(queryWord, numSug);
+//			for(String suggestion: suggestions){
+//		        System.out.println(suggestion); 
+//			}
+			reader.close();
+			spellChecker.close();
+			directory.close();
+			return suggestions;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
 	//测试
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws IOException {
 		String indexDir="C:\\Users\\asus\\Desktop\\java\\information-retrieval-system\\index";
-		String q="美利坚合众国";
+		// 处理输入
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); 
+        String str = null; 
+        System.out.println("请输入你要搜索的关键词:"); 
+        try {
+			str = br.readLine();
+			System.out.println(); 
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        // 拼写检查
+        String temp = str;
+        String[] suggestions = checkWord(str);
+        if (suggestions != null && suggestions.length != 0){
+            System.out.println("你可能想输入的是:"); 
+        	for(int i = 0; i < suggestions.length; i++){
+		        System.out.println((i+1) + " : " + suggestions[i]); 
+			}
+
+            System.out.println("请选择上面的一个正确的关键词(输入 1 ~ 5)，或继续原词(输入0)进行搜索:"); 
+            str = br.readLine();
+			System.out.println(); 
+            if (str != "0"){
+            	str = suggestions[str.charAt(0) - '1'];
+            }
+            else{
+            	str = temp;
+            }
+        }
 		
 		try {
-			search(indexDir,q);
+			search(indexDir,str);
 		} catch (Exception e) {
 		    // TODO Auto-generated catch block
 		     e.printStackTrace();
